e051d44b8212a326f1c13cb6b94003ce27adad85
Michele Lisignoli <michele@lisignoli.com>
2021-03-04 12:22:53 +0100

    JSnack 10

---
+++ JSnack_10/js/script.js
@@ -0,0 +1,7 @@
+// Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo
+var userNumber = parseInt(prompt("Please enter a number"));
+if (userNumber % 2 == 0){
+  alert("The number is even. your number:" + userNumber);
+}else {
+  alert("Your number was odd. this is right" + userNumber + 1);
+}
