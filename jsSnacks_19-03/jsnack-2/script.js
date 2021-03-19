31fda96c8a9925d0b26574dc9f6f90eb95f8e22d
Michele Lisignoli <michele@lisignoli.com>
2021-03-19 10:50:21 +0100

    falli e squadre

---
+++ jsSnacks_19-03/jsnack-2/script.js
@@ -0,0 +1,60 @@
+// Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+// 2 step:
+// Generare numeri random al posto degli 0 nelle proprietà:
+// Punti fatti e falli subiti.
+// 3 step:
+// Infine creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+
+
+
+// array di oggetti squadre
+var squadre = [
+{
+   nome: "juventus",
+   punti: "",
+   falli: ""
+},
+{
+   nome: "milan",
+   punti: "",
+   falli: "" 
+},
+{
+   nome: "roma",
+   punti: "",
+   falli: ""
+},
+{
+   nome: "atalanta",
+   punti: "",
+   falli: ""
+}
+];
+
+// generatore numrei random
+function numeroRandom(min, max) {
+   min = Math.ceil(min);
+   max = Math.floor(max);
+   return Math.floor(Math.random() * (max - min)) + min
+ }
+
+// aggiungo numero random per punti ad ogni oggetto
+for (i = 0; i < squadre.length; i++) {
+   numero = numeroRandom(1, 100);
+   squadre[i].punti = numero;
+   console.log(squadre[i].punti)
+}
+// aggiungo numero random per falli ad ogni oggetto
+for (i = 0; i < squadre.length; i++) {
+   numero = numeroRandom(1, 100);
+   squadre[i].falli = numero;
+}
+// creo un array con i punteggi finali
+var punteggi = [];
+for (i = 0; i < squadre.length; i++) {
+   punteggi.nome = squadre[i].nome;
+   punteggi.falli = squadre[i].falli;
+}
+console.log(punteggi)
